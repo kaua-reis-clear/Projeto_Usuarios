62938503eb2664151d350789ca5e33a8e0552997
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-16 09:06:14 -0300

    Projeto_Usuarios: saving user data in a Class instead a json.

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
 function addLine(dataUser) {
-  var tr = document.createElement("tr");
-
-  tr.innerHTML = `
+  console.log(dataUser);
+  var tr = (document.getElementById("table-users").innerHTML = `
     <tr>
         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
@@ -16,23 +15,23 @@ function addLine(dataUser) {
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
     </tr>
-  `;
-
-  document.getElementById("table-users").appendChild(tr);
+  `);
 }
 
-document.getElementById("form-user-create").addEventListener("submit", function (event) {
+document .getElementById("form-user-create") .addEventListener("submit", function (event) {
     event.preventDefault();
 
     fields.forEach(function (field, index) {
       if (field.name == "gender") {
         if (field.checked) {
-          user.gender;
+          user[field.name] = field.value;
         }
       } else {
         user[field.name] = field.value;
       }
     });
 
-    addLine(user);
+    var objectUser = new User( user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin );
+
+    addLine(objectUser);
   });
