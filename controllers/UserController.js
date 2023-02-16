6840500b3c6b924568fba8f87592cd20a411871b
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-16 11:50:39 -0300

    Projeto_Usuarios: using dataset to get the bool information behind a string, and using it to increase the users and admins cont.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -16,6 +16,8 @@ class UserController {
 
       let values = this.getValues();
 
+      if (!values) return false;
+
       this.getPhoto().then(
         (content) => {
           values.photo = content;
@@ -103,6 +105,8 @@ class UserController {
   addLine(dataUser) {
     let tr = document.createElement("tr");
 
+    tr.dataset.user = JSON.stringify(dataUser);
+
     tr.innerHTML = `
         <td><img src="${
           dataUser.photo
@@ -118,5 +122,23 @@ class UserController {
     `;
 
     this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  updateCount(){
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach(tr => {
+        numberUsers++;
+
+        let user = JSON.parse(tr.dataset.user);
+
+        if(user._admin) numberAdmin++;
+    });
+
+    document.querySelector("#number-users").innerHTML = numberUsers;
+    document.querySelector("#number-users-admin").innerHTML = numberAdmin;
   }
 }
