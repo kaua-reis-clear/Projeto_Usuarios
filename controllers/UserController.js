6670065b1729264c1e1f079606a213cf69a82a8d
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-16 11:30:22 -0300

    Projeto_Usuarios: using elements properties to deny an incorrect form submission.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -63,8 +63,16 @@ class UserController {
 
   getValues() {
     let user = {};
+    let isValid = true;
 
     [...this.formEl.elements].forEach(function (field, index) {
+      if (
+        ["name", "email", "password"].indexOf(field.name) > -1 &&
+        !field.value
+      ) {
+        field.parentElement.classList.add("has-error");
+        isValid = false;
+      }
       if (field.name === "gender") {
         if (field.checked) {
           user[field.name] = field.value;
@@ -75,6 +83,10 @@ class UserController {
         user[field.name] = field.value;
       }
     });
+
+    if(!isValid){
+        return false;
+    }
 
     return new User(
       user.name,
@@ -92,10 +104,12 @@ class UserController {
     let tr = document.createElement("tr");
 
     tr.innerHTML = `
-        <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+        <td><img src="${
+          dataUser.photo
+        }" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+        <td>${dataUser.admin ? "Sim" : "Não"}</td>
         <td>${Utils.dateFormat(dataUser.register)}</td>
         <td>
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
