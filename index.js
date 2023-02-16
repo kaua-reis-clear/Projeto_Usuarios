2dd7b67a361bb8643d3c01f99e6d6367167e0723
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-16 08:43:14 -0300

    Projeto_Usuarios: making the form work after saving user data in an object and displaying it on screen.

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-var nome = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
-fields.forEach(function(field, index){
+function addLine(dataUser) {
+  var tr = document.createElement("tr");
 
-    if(field.name == "gender") {
-        if(field.checked) {
-            console.log("SIM", field);
-        }  
-    } else {
-        console.log("NÃO");
-    }
+  tr.innerHTML = `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+  `;
 
-    console.log(field.id, field.name, field.value, field.checked, index);
+  document.getElementById("table-users").appendChild(tr);
+}
 
-});
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+      if (field.name == "gender") {
+        if (field.checked) {
+          user.gender;
+        }
+      } else {
+        user[field.name] = field.value;
+      }
+    });
+
+    addLine(user);
+  });
