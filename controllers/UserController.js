5604150744fa80af541c9c42ef78e8af7563eeb2
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-17 09:59:03 -0300

    Projeto_Usuarios: project refactored, with new methods since there was repetitive code blocks.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -35,8 +35,6 @@ class UserController {
 
       let result = Object.assign({}, userOld, values);
 
-      tr.dataset.user = JSON.stringify(result);
-
       this.getPhoto(this.formUpdateEl).then(
         (content) => {
           if (!values.photo) {
@@ -45,21 +43,11 @@ class UserController {
             result._photo = content;
           }
 
-          tr.innerHTML = `
-        <td><img src="${
-          result._photo
-        }" alt="User Image" class="img-circle img-sm"></td>
-        <td>${result._name}</td>
-        <td>${result._email}</td>
-        <td>${result._admin ? "Sim" : "Não"}</td>
-        <td>${Utils.dateFormat(result._register)}</td>
-        <td>
-            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-    `;
+          let user = new User();
 
-          this.addEventsTr(tr);
+          user.loadFromJSON(result);
+
+          this.getTr(user, tr);
 
           this.updateCount();
           this.formUpdateEl.reset();
@@ -214,7 +202,15 @@ class UserController {
   }
 
   addLine(dataUser) {
-    let tr = document.createElement("tr");
+    let tr = this.getTr(dataUser);    
+
+    this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  getTr(dataUser, tr = null){
+    if (tr === null) tr = document.createElement('tr');
 
     tr.dataset.user = JSON.stringify(dataUser);
 
@@ -234,9 +230,7 @@ class UserController {
 
     this.addEventsTr(tr);
 
-    this.tableEl.appendChild(tr);
-
-    this.updateCount();
+    return tr;
   }
 
   addEventsTr(tr) {
