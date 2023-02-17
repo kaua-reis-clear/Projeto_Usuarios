8eaa4e3b728cad1e63ee9f96e985d69eacdca082
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-17 09:36:29 -0300

    Projeto_Usuarios: saving user data in session storage.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,6 +6,7 @@ class UserController {
 
     this.onSubmit();
     this.onEdit();
+    this.selectAll();
   }
 
   onEdit() {
@@ -88,6 +89,8 @@ class UserController {
       this.getPhoto(this.formCreateEl).then(
         (content) => {
           values.photo = content;
+
+          this.insert(values);
 
           this.addLine(values);
 
@@ -180,6 +183,34 @@ class UserController {
       user.photo,
       user.admin
     );
+  }
+
+  getUsersStorage(){
+    let users = [];
+
+    if(sessionStorage.getItem("users")){
+      users = JSON.parse(sessionStorage.getItem("users"))
+    }
+
+    return users;
+  }
+
+  selectAll(){
+    let users = this.getUsersStorage();
+
+    users.forEach(dataUser => {
+      let user = new User();
+      user.loadFromJSON(dataUser);
+      this.addLine(user);
+    })
+  }
+
+  insert(data){
+    let users = this.getUsersStorage();
+
+    users.push(data);
+
+    sessionStorage.setItem("users", JSON.stringify(users));
   }
 
   addLine(dataUser) {
